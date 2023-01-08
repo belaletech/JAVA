7f0457ec106f2956d1a471a2ff8df48f5a19c6c2
belaletech <belalahmadswnbhr@gmail.com>
2023-01-08 18:36:09 +0530

    update

---
+++ src/basic.java
@@ -0,0 +1,17 @@
+public class basic {
+    public static void main(String[]args)
+    {
+        int j=0;
+        int [] ar={20,22,30,15,16,12};
+        for (int i=0;i<ar.length-1;i++)
+        {
+            for( j=j+1; j<ar.length;j++)
+            {
+                if((ar[i]==ar[j])&&(i !=j))
+                {
+                    System.out.println("Duplicate Element is "+ar[j]);
+                }
+            }
+        }
+    }
+}
