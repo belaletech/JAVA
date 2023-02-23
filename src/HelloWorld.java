60f119c467fab865a89168442dfa6bf68b518118
belaletech <belalahmadswnbhr@gmail.com>
2023-02-23 15:29:26 +0530

    updated first of java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[]args)
     {
         System.out.println("This side belal do you know");
+        System.out.println("java by kunal kushwaha");
     }
 }
